ea975e0c40da61df9460f3496f8ec2206cf70c5d
BestPoovarasan <poovarasan.official@gmail.com>
2022-09-02 15:58:02 +0530

    zoom server

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/', (req, res) => {
         const match = await bcryptjs.compare(req.body.password, user.password);
         if (match) {
           // <----------Json Web Token------------------------------------->
-          const token = jwt.sign({_id : user._id,}, SECRET);
+          const token = jwt.sign({_id : user._id}, SECRET);
           res.json({
             message: "suceessfully Login",
             token,
